04d8c13a60d0cce4a7abb430030cb79d05e62386
RELHAMRI <elh.radouane@gmail.com>
2024-10-04 11:43:51 +0200

    voir les taches non assignées

--- src/Views/home.view.php
+++ src/Views/home.view.php
@@ -1,45 +1,50 @@
 <?php
 require_once(__DIR__ . '/partials/head.php');
 ?>
-
-<section class="hero">
-    <div class="hero-content">
-
-        <?php
-        if (isset($_SESSION['user'])) {
-        ?>
-            <h1>Bienvenue à toi</h1>
-            <p>Tu est maintenant connecté !</p>
-        <?php
-        } else {
-        ?>
-            <h1>Bienvenue sur Mon Site</h1>
-            <p>Inscrivez-vous dès aujourd'hui et profitez de toutes nos fonctionnalités exclusives !</p>
-            <a href="/register" class="cta-btn">Commencez Maintenant</a>
-        <?php
-        }
-        ?>
-
-    </div>
-</section>
-
-<section class="features">
-    <div class="container">
-        <div class="feature">
-            <h3>Fonctionnalité 1</h3>
-            <p>Profitez d'une expérience utilisateur unique, pensée pour vous.</p>
-        </div>
-        <div class="feature">
-            <h3>Fonctionnalité 2</h3>
-            <p>Accédez à du contenu exclusif et à des outils innovants.</p>
-        </div>
-        <div class="feature">
-            <h3>Fonctionnalité 3</h3>
-            <p>Rejoignez une communauté grandissante d'utilisateurs satisfaits.</p>
-        </div>
-    </div>
-</section>
-
+<h1>Task'N'Kids</h1>
 
 <?php
+if (isset($_SESSION['user']) && $_SESSION['user']['idRole'] == 1) {
+?>
+    <h2>Tâche à venir non assignée</h2>
+    <?php
+    if (isset($arrayTasks)) {
+        foreach ($arrayTasks as $task) {
+            $dateStartDay = date_create($task->getStartTask());
+            $dateStopDay = date_create($task->getStopTask());
+    ?>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $task->getTitle() ?></h5>
+                    <p class="card-text"><?= $task->getContent() ?></p>
+                    <p class="card-text">Du <?= date_format($dateStartDay, 'd-m-Y à H:i') ?> au <?= date_format($dateStopDay, 'd-m-Y à H:i') ?></p>
+                    <a href="/task?id=<?= $task->getId() ?>" class="btn btn-primary">Voir plus</a>
+                </div>
+            </div>
+    <?php
+        }
+    }
+    ?>
+    <h2>Tâche à venir assignée</h2>
+    <?php
+    if (isset($arrayTasksByUsers)) {
+        foreach ($arrayTasksByUsers as $task) {
+            $dateStartDay = date_create($task->getStartTask());
+            $dateStopDay = date_create($task->getStopTask());
+    ?>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $task->getTitle() ?></h5>
+                    <p class="card-text"><?= $task->getContent() ?></p>
+                    <p class="card-text">Du <?= date_format($dateStartDay, 'd-m-Y à H:i') ?> au <?= date_format($dateStopDay, 'd-m-Y à H:i') ?></p>
+                    <p class="card-text">Assigné à : <?= $task->getPseudo() ?></p>
+                    <a href="/task?id=<?= $task->getId() ?>" class="btn btn-primary">Voir plus</a>
+                </div>
+            </div>
+    <?php
+        }
+    }
+    ?>
+<?php
+}
 require_once(__DIR__ . '/partials/footer.php');

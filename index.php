61c68842c61264b207d37c83e2b8b55b95957a9f
RELHAMRI <elh.radouane@gmail.com>
2024-10-07 11:00:51 +0200

    afficher une tache

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <?php
 require "vendor/autoload.php";
 session_start();
+
+use App\Controllers\TaskController;
 use Config\Router;
 
 $router = new Router();
 
-/**
- * J'utilise la méthode addRoute de mon controller pour ajouter des routes au controller
- * Cette méthode prend trois arguments, la route, le controllers et la méthode executée
+/** j'utilise la methode addRouute de mon controller pour ajouter des routes au controller
+ *  cette methode prends trois argument, la route, le controller et la methode executé
  */
-
+//la page d'accueil
 $router->addRoute('/', 'HomeController', 'index');
+//La connexion/deconnexion et inscription:
 $router->addRoute('/register', 'RegisterController', 'index');
 $router->addRoute('/login', 'LoginController', 'index');
 $router->addRoute('/logout', 'LogoutController', 'logout');
+//Les tâches:
 $router->addRoute('/addTask', 'TaskController', 'createTask');
+$router->addRoute('/task', 'TaskController', 'index');
+
 $router->handleRequest();
